e1daed20bbeb8fe5d03b2e5968d3270c55b50846
Andrea70031 <andreaferrari97@gmail.com>
2026-09-08 18:49:39 +0200

    Initialize Google Mobile Ads SDK

--- SonicMD/SonicMDApp.swift
+++ SonicMD/SonicMDApp.swift
@@ -1,8 +1,13 @@
 import SwiftUI
+import GoogleMobileAds
 
 @main
 struct SonicMDApp: App {
     @StateObject private var engine = AcousticEngine()
+
+    init() {
+        MobileAds.shared.start()
+    }
 
     var body: some Scene {
         WindowGroup {
